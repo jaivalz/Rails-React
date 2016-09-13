469346b52cd7a0b26f6b157d06e8e2222cb44742
Jaime <jaivalz@yahoo.com>
2016-09-13 16:11:19 +0000

    Edited restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,5 +3,9 @@ class RestaurantsController < ApplicationController
   def index
      @restaurants = Restaurant.all   
   end
+  
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
 end
