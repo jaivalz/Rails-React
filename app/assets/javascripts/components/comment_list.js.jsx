eca3e4f09cb3c64c8981101a536f7a8dc3c45397
Jaime <jaivalz@yahoo.com>
2016-09-17 12:24:02 +0000

    Modified appjs flux to appes6

--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -1,24 +1,25 @@
 var CommentList = React.createClass({
 
   componentDidMount: function() {
-    Store.addChangeListener(this._onChange);
+    commentStore.addChangeListener(this._onChange);
   },
-  
+
   componentWillUnmount: function() {
-    Store.removeChangeListener(this._onChange);
+    commentStore.removeChangeListener(this._onChange);
   },
   
+
   render: function() {
-    return ( 
+    return (
       <div>
-        {Store.comments().map(function(comment) {
+        {commentStore.comments().map(function(comment) {
           return <Comment key={comment.id} {... comment} />;
         })}
       </div>
-    );
+      );
   },
-  
+
   _onChange: function() {
     this.forceUpdate();
   }
-});
+}); 
